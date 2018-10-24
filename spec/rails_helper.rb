90aa383fc36b8834dd707232c1e32c02f37201a9
Vladislav Isakov <bladebhs@gmail.com>
2018-10-24 23:08:50 +0500

    Remove uploaded files after tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,4 +22,10 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.after(:all) do
+    if Rails.env.test? 
+      FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+    end 
+  end
 end
