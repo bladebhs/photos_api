5be29a425d561334bb7df0944a25a5e7709b3021
Vladislav Isakov <bladebhs@gmail.com>
2018-10-25 21:47:08 +0500

    Add authenticated_header helper

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
 module RequestSpecHelper
   def json
     JSON.parse(response.body)
+  end
+
+  def authenticated_header
+    user = User.create(email: 'test@test.com', password: '12345678')
+    token = Knock::AuthToken.new(payload: { sub: user.id }).token
+
+    {
+      'Authorization': "Bearer #{token}"
+    }
   end
 end
