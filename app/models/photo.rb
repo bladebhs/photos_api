17bf54e18e6f33a2daaa95e9140d8672da93d66f
Vladislav Isakov <bladebhs@gmail.com>
2018-10-25 17:24:36 +0500

    Add uploader and validation to Photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,13 @@
 class Photo < ApplicationRecord
+  mount_uploader :file, PhotoUploader
+
+  validates :file, file_size: { less_than_or_equal_to: 5.megabytes }
+
+  def original
+    file.url
+  end
+
+  def preview
+    file.preview.url
+  end
 end
