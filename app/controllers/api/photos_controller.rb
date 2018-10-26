53bac891f23e30c9a231691d7a40f7268cbf31d0
Vladislav Isakov <bladebhs@gmail.com>
2018-10-26 17:41:03 +0500

    Add PhotosController

---
+++ app/controllers/api/photos_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Api
+  class PhotosController < ApplicationController
+    before_action :authenticate_user
+
+    def index
+      photos = paginate Photo.all
+      render json: PhotoSerializer.new(photos).serialized_json
+    end
+
+    def create
+      photo = Photo.create!(photo_params)
+      ExifWorker.perform_async(photo)
+      render json: photo, status: :created
+    end
+
+    private
+
+    def photo_params
+      params.require(:photo).permit(:file)
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      render json: { message: e.message }, status: :unprocessable_entity
+    end
+  end
+end
