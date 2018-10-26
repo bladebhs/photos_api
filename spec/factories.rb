7295568c42d4e9a11eee14dd3b7fced0854e1c07
Vladislav Isakov <bladebhs@gmail.com>
2018-10-26 16:41:48 +0500

    Add user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 include ActionDispatch::TestProcess
 
 FactoryBot.define do
   factory :photo do
-    file { fixture_file_upload "spec/fixtures/files/#{[1,2,3].sample}.jpg", 'image/jpeg' }
+    filename = "spec/fixtures/files/#{[1, 2, 3].sample}.jpg"
+    file { fixture_file_upload filename, 'image/jpeg' }
+  end
+
+  factory :user do
+    email { 'test@test.com' }
+    password { '12345678' }
   end
 end
